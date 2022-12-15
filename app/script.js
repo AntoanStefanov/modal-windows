9d028aab5fc48f96fa928ad19d76e487b2705238
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-15 10:49:13 +0200

    adding event handler, next step depends on classes

--- app/script.js
+++ app/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+/**
+ * @return {void} activating Model.
+ */
+function activateModal() {
+  const body = document.querySelector('body');
+  body.addEventListener('click', (ev) => {
+    const className = ev.target.className;
+    console.log(className);
+
+    if (className === 'show-modal') {
+      console.log('Modal for showing');
+    } else if (className === 'close-modal') {
+      console.log('Modal for closing');
+    }
+  });
+}
+
+activateModal();
