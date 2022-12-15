8cbe946db0b56e4631c860562ab34841e27316dd
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-15 13:00:25 +0200

    adding second SOLUTION, which better tho?

--- app/script.js
+++ app/script.js
@@ -4,28 +4,54 @@ const addRemoveClass = (el, _class, toAdd = true) => {
   toAdd ? el.classList.add(_class) : el.classList.remove(_class);
 };
 
+const addRemoveClassArray = (elArray, _class, toAdd = true) => {
+  for (const el of elArray) {
+    addRemoveClass(el, _class, toAdd);
+  }
+};
+
 /**
  * @return {void} activating Model.
  */
 function activateModal() {
-  const body = document.querySelector('body');
+  // const body = document.querySelector('body');
+  const modal = document.querySelector('.modal');
+  const overlay = document.querySelector('.overlay');
 
+  const btnCloseModal = document.querySelector('.close-modal');
+  const btnsShowModal = document.querySelectorAll('.show-modal');
+
+  const showModal = () => {
+    addRemoveClassArray([modal, overlay], 'hidden', false);
+  };
+
+  const hideModal = () => {
+    addRemoveClassArray([modal, overlay], 'hidden');
+  };
+  /*
   body.addEventListener('click', (ev) => {
     const className = ev.target.className;
     const tagName = ev.target.tagName;
 
     if (tagName.toLowerCase() === 'button') {
-      const modal = document.querySelector('.modal');
-      const overlay = document.querySelector('.overlay');
-
       if (className === 'show-modal') {
-        addRemoveClass(modal, 'hidden', false);
-        addRemoveClass(overlay, 'hidden', false);
+        showModal();
       } else if (className === 'close-modal') {
-        addRemoveClass(modal, 'hidden');
-        addRemoveClass(overlay, 'hidden');
+        hideModal();
       }
     }
+  });
+  */
+
+  // This one or above ?
+  for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', function (e) {
+      showModal();
+    });
+  }
+
+  btnCloseModal.addEventListener('click', (ev) => {
+    hideModal();
   });
 }
 
