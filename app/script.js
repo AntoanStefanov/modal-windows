db5b89f51e083373b161e6f1d0c91b814c80f8f5
AntoanStefanov <antoine.stefanov@gmail.com>
2022-12-15 10:54:32 +0200

    refactoring needed

--- app/script.js
+++ app/script.js
@@ -7,12 +7,19 @@ function activateModal() {
   const body = document.querySelector('body');
   body.addEventListener('click', (ev) => {
     const className = ev.target.className;
+    console.log(ev.target.tagName);
     console.log(className);
 
     if (className === 'show-modal') {
       console.log('Modal for showing');
+      const hiddenModal = document.querySelector('.modal.hidden');
+      hiddenModal.classList.remove('hidden');
+      document.querySelector('.overlay').classList.remove('hidden');
     } else if (className === 'close-modal') {
       console.log('Modal for closing');
+      const showedModal = document.querySelector('.modal');
+      showedModal.classList.add('hidden');
+      document.querySelector('.overlay').classList.add('hidden');
     }
   });
 }
